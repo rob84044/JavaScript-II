1cacaec4bf97503dc2bdfaef05b8e38b082985dc
Robert Pedersen <rob84044@users.noreply.github.com>
2019-03-27 21:23:30 -0600

    Completed array-methods
    I completed the array methods file

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -52,7 +52,7 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 {"id":48,"first_name":"Anderea","last_name":"MacGiolla Pheadair","email":"amacgiollapheadair1b@xing.com","shirt_size":"2XL","company_name":"Kwimbee","donation":214},
 {"id":49,"first_name":"Bel","last_name":"Alway","email":"balway1c@ow.ly","shirt_size":"S","company_name":"Voolia","donation":107},
 {"id":50,"first_name":"Shell","last_name":"Baine","email":"sbaine1d@intel.com","shirt_size":"M","company_name":"Gabtype","donation":171}];
-
+/*
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
@@ -95,24 +95,16 @@ runners.filter((runner) => {
 //Print out to the console the full array of LargeShirts
 console.log(largeShirts);
 
-
+*/
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 
 let ticketPriceTotal = [];
 
-totalCost = (acc, cur) => acc + cur;
+ticketPriceTotal = runners.reduce((total, current) => total + current.donation, 0)
 
-runners.reduce((ticket) => {
- if (ticket.donation = ""){
-  //Leave empty
- }else {
-  
- }
-})
-
-console.log(ticketPriceTotal);
+console.log('The total raised value is $'+ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
